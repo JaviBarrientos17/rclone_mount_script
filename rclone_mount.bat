dcc9f94be4c9359dde54751174d8f819629ed2d9
JaviBarrientos17 <javierbarrientos2010@gmail.com>
2023-03-07 18:04:15 +0100

    Added comments

--- rclone_mount.bat
+++ rclone_mount.bat
@@ -1,9 +1,11 @@
 @echo off
 
-rem Define the folder path
+rem Define the folder path of your rclone
 set "FOLDER_PATH=C:\Users\javie\Documents\rclone-v1.61.1-windows-amd64"
 
 rem Load the command in the Windows terminal
 cmd.exe /c "cd %FOLDER_PATH% && rclone mount --vfs-cache-mode full mystuff:/ k:"
 
 pause
+
+rem Read the README file to know how to load this script on windows startup
